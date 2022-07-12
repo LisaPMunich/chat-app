c1eb0e4eb2d158e704ef53a17879b752c798640d
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-07-12 12:01:18 +0200

    feat: Extract config values into dedicated ignored file

--- config/firebase.js
+++ config/firebase.js
@@ -2,17 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-
-
-// adding firebase credentials in order to connect to firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyArgDfYVs3pi5LBlMh_qXU1o-q_FtAbCMw",
-    authDomain: "chatapp-6f894.firebaseapp.com",
-    projectId: "chatapp-6f894",
-    storageBucket: "chatapp-6f894.appspot.com",
-    messagingSenderId: "463623490740",
-    appId: "1:463623490740:web:bf47e4df6231c866ad1be1",
-};
+import {firebaseConfig} from "./firebaseConfig";
 
 
 // initialize firebase
@@ -27,4 +17,8 @@ export const auth = getAuth();
 
 // Initialize Cloud Storage and get a reference to the service
 export const storage = getStorage(firebaseApp);
+
+
+
+
 
---
+++ config/firebaseConfig.dist.js
@@ -0,0 +1,13 @@
+/**
+ * Copy this file to firebaseConfig.js with your config values
+ */
+
+// adding firebase credentials in order to connect to firebase
+export const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "PROJECT.firebaseapp.com",
+    projectId: "PROJECT",
+    storageBucket: "PROJECT.appspot.com",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID",
+};
